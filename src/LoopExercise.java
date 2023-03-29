112bff0d3b9af5bf5fcacf23a5171fa86b6575ac
Samuel Chijioke Onyi <ceejeek@yahoo.com>
2023-03-29 13:36:30 +0100

    push repo

---
+++ src/LoopExercise.java
@@ -0,0 +1,17 @@
+public class LoopExercise {
+    public static void main(String[] args){
+
+        for(int i=0; i<=10; i++){
+            System.out.println(i + "=kick the enemy");
+        }
+                for (int i = 2; i<20; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(i);
+                    }
+                    for (int x = 10; x>=1; x--){
+                        System.out.println("print no 10 to 1!");
+                        System.out.println("print x");
+                    }
+                }
+            }
+        }
